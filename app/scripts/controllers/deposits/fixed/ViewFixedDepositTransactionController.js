3d918d98f61b53d3b006584c84730838723f0931
Frank Nkuyahaga <frank@fiter.io>
2019-10-17 11:48:01 +0300

    Revert interest posting transaction undo changes

--- app/scripts/controllers/deposits/fixed/ViewFixedDepositTransactionController.js
+++ app/scripts/controllers/deposits/fixed/ViewFixedDepositTransactionController.js
@@ -1,6 +1,6 @@
 (function (module) {
     mifosX.controllers = _.extend(module, {
-        ViewFixedDepositTransactionController: function (scope, resourceFactory, location, routeParams, dateFilter, $uibModal) {
+        ViewFixedDepositTransactionController: function (scope, resourceFactory, location, routeParams, dateFilter) {
             scope.flag = false;
             resourceFactory.fixedDepositTrxnsResource.get({savingsId: routeParams.accountId, transactionId: routeParams.transactionId}, function (data) {
                 scope.transaction = data;
@@ -9,55 +9,17 @@
                 }
             });
 
-			scope.undo = function (accountId, transactionId) {
-				$uibModal.open({
-					templateUrl: 'undotransaction.html',
-					controller: UndoTransactionModel,
-					resolve: {
-						accountId: function () {
-							return accountId;
-						},
-						transactionId: function () {
-							return transactionId;
-						}
-					}
-				});
-			};
-
-			var UndoTransactionModel = function ($scope, $uibModalInstance, accountId, transactionId) {
-				$scope.undoTransaction = function () {
-					var params = {savingsId: accountId, transactionId: transactionId, command: 'undo'};
-					var formData = {dateFormat: scope.df, locale: scope.optlang.code, transactionAmount: 0};
-					formData.transactionDate = dateFilter(new Date(), scope.df);
-					resourceFactory.fixedDepositTrxnsResource.save(params, formData, function (data) {
-						location.path('/viewfixeddepositaccount/' + data.savingsId);
-					});
-				};
-//				$scope.undoTransaction = function () {
-//					var params = {savingsId: accountId, transactionId: transactionId, command: 'undo'};
-//					var formData = {dateFormat: scope.df, locale: scope.optlang.code, transactionAmount: 0};
-//					formData.transactionDate = dateFilter(new Date(), scope.df);
-//					resourceFactory.savingsTrxnsResource.save(params, formData, function (data) {
-//						$uibModalInstance.close('delete');
-//						location.path('/viewsavingaccount/' + data.savingsId);
-//					});
-//				};
-				$scope.cancel = function () {
-					$uibModalInstance.dismiss('cancel');
-				};
-			};
-
-//            scope.undoTransaction = function (accountId, transactionId) {
-//                var params = {savingsId: accountId, transactionId: transactionId, command: 'undo'};
-//                var formData = {dateFormat: scope.df, locale: scope.optlang.code, transactionAmount: 0};
-//                formData.transactionDate = dateFilter(new Date(), scope.df);
-//                resourceFactory.fixedDepositTrxnsResource.save(params, formData, function (data) {
-//                    location.path('/viewfixeddepositaccount/' + data.savingsId);
-//                });
-//            };
+            scope.undoTransaction = function (accountId, transactionId) {
+                var params = {savingsId: accountId, transactionId: transactionId, command: 'undo'};
+                var formData = {dateFormat: scope.df, locale: scope.optlang.code, transactionAmount: 0};
+                formData.transactionDate = dateFilter(new Date(), scope.df);
+                resourceFactory.fixedDepositTrxnsResource.save(params, formData, function (data) {
+                    location.path('/viewfixeddepositaccount/' + data.savingsId);
+                });
+            };
         }
     });
-    mifosX.ng.application.controller('ViewFixedDepositTransactionController', ['$scope', 'ResourceFactory', '$location', '$routeParams', 'dateFilter', '$uibModal', mifosX.controllers.ViewFixedDepositTransactionController]).run(function ($log) {
+    mifosX.ng.application.controller('ViewFixedDepositTransactionController', ['$scope', 'ResourceFactory', '$location', '$routeParams', 'dateFilter', mifosX.controllers.ViewFixedDepositTransactionController]).run(function ($log) {
         $log.info("ViewFixedDepositTransactionController initialized");
     });
 }(mifosX.controllers || {}));
